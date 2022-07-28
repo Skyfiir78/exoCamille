af1e7b6a45734fd2a1c8440d535541225871f4a4
Steven <sammarone.steven@gmail.com>
2022-07-28 11:00:02 +0200

    exo 4 v2

--- exo4.js
+++ exo4.js
@@ -9,19 +9,17 @@ const generateRandomArray = require("./utils/generateRandomArray.js")
 */
 
 //Tableau a utiliser
-const arr = generateRandomArray()
+const arr = [1,4,5,5,2,5,7,3,1,3,4,6,6,4,2,5,7,34,2,56]
 
 
 
 const countInArray = (array, elementToCount) => {
-
-
   let foundElement = array.find(element => elementToCount === elementToCount);
 
   return foundElement;
 
 }
 
-let result = countInArray(arr, 9);
+let result = countInArray(arr, 6);
 
 console.log(result);
